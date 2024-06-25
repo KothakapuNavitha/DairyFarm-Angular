8a5342ea70bec786ad2f80547b46194ded2187f8
KalakondaNihirika <nihirikakalakonda2000@gmail.com>
2024-06-25 16:52:29 +0530

    changes by niharika

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientDetailsComponent } from './client-details/client-details.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CattleDetailsComponent } from './cattle-details/cattle-details.component';
+import { ProductionComponent } from './production/production.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: 
 Routes = [
-  {path: '',  component:HomePageComponent},
   {path:'homepage',component:HomePageComponent},
   {path:'clientDetails',component:ClientDetailsComponent},
-  {path:'CattleDetails',component:CattleDetailsComponent}
+  {path:'CattleDetails',component:CattleDetailsComponent},
+  {path:'Production',component:ProductionComponent},
+  {path:'Products',component:ProductDetailsComponent},
+
 ];
 
 @NgModule({
